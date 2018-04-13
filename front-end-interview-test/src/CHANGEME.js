a3141500683d2104242e6ca758cd9f001766ca24
blaws006 <blaws006>
2018-04-13 18:04:16 -0400

    initializing front end input

--- front-end-interview-test/src/CHANGEME.js
+++ front-end-interview-test/src/CHANGEME.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
+import API from './API'
 
 class Test extends Component {
     render() {
         return (
             <div className="testContainer">
                 <div className="filterContainer">
-                    Your filters go here.
+                   <h3>Beds</h3>
+                   <input name="maxbeds" placeholder="Max"/><br /><br />
+                   <input name = "minbeds"  placeholder = "Min" />
+                   <h3>Baths</h3> 
+                   <input name = "maxbeds" placeholder = "Max"/><br/><br/>
+                    <input name = "minbeds" placeholder = "Min" /><br /><br />
+                    <button>Submit</button>
                 </div>
                 <RemineTable properties={[]} />
             </div>
